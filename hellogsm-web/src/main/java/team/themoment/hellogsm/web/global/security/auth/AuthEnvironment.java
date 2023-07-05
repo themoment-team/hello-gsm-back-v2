47b0605298340974c8f9ede9290baef9388255f2
YangSijun528 <yangsijun5528@gmail.com>
2023-07-06 00:13:10 +0900

    build: docker 및 docker-compose 관련 설정

--- hellogsm-web/src/main/java/team/themoment/hellogsm/web/global/security/auth/AuthEnvironment.java
+++ hellogsm-web/src/main/java/team/themoment/hellogsm/web/global/security/auth/AuthEnvironment.java
@@ -3,6 +3,7 @@ package team.themoment.hellogsm.web.global.security.auth;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
+import java.util.Objects;
 
 
 /**
@@ -13,8 +14,48 @@ import java.util.List;
  */
 
 @ConfigurationProperties(prefix = "auth")
-public record AuthEnvironment(
-        String redirectBaseUri,
-        List<String> allowedOrigins
-) {
+public final class AuthEnvironment {
+    private final String redirectBaseUri;
+    private final List<String> allowedOrigins;
+
+    /**
+     *
+     */
+    public AuthEnvironment(
+            String redirectBaseUri,
+            String allowedOrigins
+    ) {
+        this.redirectBaseUri = redirectBaseUri;
+        this.allowedOrigins = List.of(allowedOrigins.split(","));
+    }
+
+    public String redirectBaseUri() {
+        return redirectBaseUri;
+    }
+
+    public List<String> allowedOrigins() {
+        return allowedOrigins;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (AuthEnvironment) obj;
+        return Objects.equals(this.redirectBaseUri, that.redirectBaseUri) &&
+                Objects.equals(this.allowedOrigins, that.allowedOrigins);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(redirectBaseUri, allowedOrigins);
+    }
+
+    @Override
+    public String toString() {
+        return "AuthEnvironment[" +
+                "redirectBaseUri=" + redirectBaseUri + ", " +
+                "allowedOrigins=" + allowedOrigins + ']';
+    }
+
 }
